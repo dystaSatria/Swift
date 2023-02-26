f256084d901d3121419788af4ddf6f99de138f88
REZA DYSTA SATRIA <87264688+dyzzta@users.noreply.github.com>
2023-02-26 21:45:33 +0300

    Add files via upload

---
+++ week_1/factorial_func.swift
@@ -0,0 +1,28 @@
+func factorial_for(_ x: Int) -> Int {
+    var sonuc_for = 1
+    for i in 1...x {
+        sonuc_for *= i
+    }
+    return sonuc_for
+}
+
+func factorial_while(_ y: Int) -> Int {
+    var sonuc_while = 1
+    var i = 1
+    while i <= y {
+        sonuc_while *= i
+        i += 1
+    }
+    return sonuc_while
+}
+print("for ile faktöriyel fonksiyonu için sayi girin :\n")
+var sayi_for = Int(readLine()!)!
+var sonuc_for = factorial_for(sayi_for) // returns 120
+print("Sonucu for ise  \(sonuc_for)\n")
+
+print("while ile faktöriyel fonksiyonu için sayi girin :\n")
+var sayi_while = Int(readLine()!)!
+var sonuc_while = factorial_while(sayi_while) // returns 120
+print("Sonucu while ise \(sonuc_while)\n")
+
+
