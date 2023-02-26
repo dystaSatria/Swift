5c2ad6b8e423b4c3e145143c51717608d5e9d008
REZA DYSTA SATRIA <87264688+dyzzta@users.noreply.github.com>
2023-02-26 22:17:51 +0300

    Add files via upload

---
+++ week_1/ters_sayi.swift
@@ -0,0 +1,19 @@
+func ters_sayi(_ sayi: Int) -> Int {
+    var result = 0
+    var num = sayi
+    
+    while num != 0 {
+        var hane = num % 10
+        result = result * 10 + hane
+        num /= 10
+    }
+    
+    return result
+}
+
+
+print("Sayi girin :\n")
+var sayi = Int(readLine()!)!
+var sonuc = ters_sayi(sayi)
+print("Ters sayi \(sayi), \(sonuc)")
+    
