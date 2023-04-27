52f7052e100872bdf33680232fa1124d74eb92c5
REZA DYSTA SATRIA <87264688+dyzzta@users.noreply.github.com>
2023-04-27 23:41:42 +0300

    Add files via upload

---
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Reza Dysta SATRIA on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var c3: UIButton!
+    @IBOutlet weak var c2: UIButton!
+    @IBOutlet weak var c1: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var a3: UIButton!
+    @IBOutlet weak var a2: UIButton!
+    @IBOutlet weak var a1: UIButton!
+    @IBOutlet weak var turnLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func boardTapAction(_ sender: UIButton) {
+    }
+}
+
