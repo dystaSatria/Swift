cbf74dcceedd42a93b9119adbb4badcdaada32c9
REZA DYSTA SATRIA <87264688+dyzzta@users.noreply.github.com>
2023-04-27 22:39:58 +0300

    Add files via upload

---
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Reza Dysta SATRIA on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
